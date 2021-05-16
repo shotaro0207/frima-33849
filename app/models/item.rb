c4b2659135eabba555e67117d37446cff060a0c6
shotaro0207 <shotaro9727@gmail.com>
2021-05-16 09:43:06 +0900

    出品機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,32 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :state
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :day
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :item_name
+    validates :description
+    validates :category_id
+    validates :state_id
+    validates :delivery_fee_id
+    validates :prefectures_id
+    validates :days_id
+    validates :price
+    validates :image
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :state_id
+    validates :delivery_fee_id
+    validates :prefectures_id
+    validates :days_id
+  end
+
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
 end
