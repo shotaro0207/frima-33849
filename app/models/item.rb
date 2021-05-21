cdc0563b34916b7131b99029aba7f55d183230ae
shotaro0207 <shotaro9727@gmail.com>
2021-05-21 17:56:58 +0900

    商品詳細表示機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,8 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :state
   belongs_to :delivery_fee
-  belongs_to :prefecture
-  belongs_to :day
+  belongs_to :prefectures
+  belongs_to :days
   belongs_to :user
   has_one_attached :image
 
